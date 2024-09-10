7d663467d4a64863024068be6b301078084ac8fa
drwhut <drwhut@gmail.com>
2024-09-10 20:05:05 +0100

    Started work on the new asset menu!
    Now there's these cool things called folders! It's a totally new
    invention, I think it'll do really well with those tiktok kids.
    
    The way in which assets are displayed can also be adjusted now, with
    both the button type and scale being variable.
    
    This is primarily to fix #268, as the 3D previews were causing
    random crashes in v0.1.x when going through the pages.
    
    This also fixes #299, as tooltips now only show descriptions. I plan
    to add a right-click option to show the full details of an asset.
    
    This commit also contributes to #252, since tokens are now shown in
    the UI as "Tiles / Tokens". The ability for tokens to be placed in
    player's hands has not been implemented yet.

---
+++ game/ui/asset_menu/asset_button.gd
@@ -0,0 +1,200 @@
+# tabletop-club
+# Copyright (c) 2020-2024 Benjamin 'drwhut' Beddows.
+# Copyright (c) 2021-2024 Tabletop Club contributors (see game/CREDITS.tres).
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+class_name AssetButton
+extends Control
+
+## A button with multiple appearances that represents an asset in the AssetDB.
+
+
+## Fired when the button is pressed.
+signal pressed()
+
+
+## The ways in which the button can appear.
+enum ButtonType {
+	VERTICAL,
+	HORIZONTAL,
+}
+
+
+## The size of the detail icons in the top-right corner.
+const DETAIL_ICON_SIZE := Vector2(20.0, 20.0)
+
+## The margin given to the detail icons relative to the edge of the button.
+const DETAIL_ICON_MARGIN := 2.0
+
+
+## Sets what the button looks like.
+export(ButtonType) var appearance: int setget set_appearance, get_appearance
+
+## Sets the text that the button displays.
+## TODO: Do we want the text to be able to scroll? Or do we rely on being able
+## to show the entire text?
+export(String) var text: String setget set_text, get_text
+
+## Sets the icon that the button displays.
+export(Texture) var texture: Texture setget set_texture, get_texture
+
+## Set the hint that is displayed when the button is hovered over.
+export(String) var hint: String setget set_hint, get_hint
+
+## Set the font used to render the button text.
+export(Font) var font_override: Font setget set_font_override, get_font_override
+
+## Set if the button represents a folder.
+export(bool) var folder: bool setget set_folder, is_folder
+
+## Set if the button represents a stack.
+export(bool) var stack: bool setget set_stack, is_stack
+
+
+# The button that is used when in vertical mode.
+var _button_vertical: VerticalButton = null
+
+# The button that is used when in horizontal mode.
+var _button_horizontal: Button = null
+
+# The icon that shows the button represents a folder.
+var _folder_icon: TextureRect = null
+
+# The icon that shows the button represents a stack.
+var _stack_icon: TextureRect = null
+
+
+func _init():
+	_button_vertical = VerticalButton.new()
+	_button_vertical.anchor_right = 1
+	_button_vertical.anchor_bottom = 1
+	_button_vertical.connect("pressed", self, "_on_button_pressed")
+	add_child(_button_vertical)
+	
+	_button_horizontal = Button.new()
+	_button_horizontal.clip_text = true
+	_button_horizontal.expand_icon = true
+	_button_horizontal.anchor_right = 1
+	_button_horizontal.anchor_bottom = 1
+	_button_horizontal.visible = false
+	_button_horizontal.connect("pressed", self, "_on_button_pressed")
+	add_child(_button_horizontal)
+	
+	var icon_container := HBoxContainer.new()
+	icon_container.anchor_left = 1
+	icon_container.anchor_right = 1
+	icon_container.margin_top = DETAIL_ICON_MARGIN
+	icon_container.margin_right = -DETAIL_ICON_MARGIN
+	icon_container.grow_horizontal = Control.GROW_DIRECTION_BEGIN
+	icon_container.mouse_filter = Control.MOUSE_FILTER_IGNORE
+	add_child(icon_container)
+	
+	_folder_icon = TextureRect.new()
+	_folder_icon.texture = preload("res://icons/folder_icon.svg")
+	_folder_icon.expand = true
+	_folder_icon.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_COVERED
+	_folder_icon.rect_min_size = DETAIL_ICON_SIZE
+	_folder_icon.mouse_filter = Control.MOUSE_FILTER_IGNORE
+	_folder_icon.visible = false
+	icon_container.add_child(_folder_icon)
+	
+	_stack_icon = TextureRect.new()
+	_stack_icon.texture = preload("res://icons/stack_icon.svg")
+	_stack_icon.expand = true
+	_stack_icon.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_COVERED
+	_stack_icon.rect_min_size = DETAIL_ICON_SIZE
+	_stack_icon.mouse_filter = Control.MOUSE_FILTER_IGNORE
+	_stack_icon.visible = false
+	icon_container.add_child(_stack_icon)
+
+
+func get_appearance() -> int:
+	if _button_vertical.visible:
+		return ButtonType.VERTICAL
+	elif _button_horizontal.visible:
+		return ButtonType.HORIZONTAL
+	
+	return 0
+
+
+func get_text() -> String:
+	# Any of the buttons should work fine here.
+	return _button_horizontal.text
+
+
+func get_texture() -> Texture:
+	return _button_horizontal.texture
+
+
+func get_hint() -> String:
+	return _button_horizontal.hint_tooltip
+
+
+func get_font_override() -> Font:
+	return _button_horizontal.get_font("font")
+
+
+func is_folder() -> bool:
+	return _folder_icon.visible
+
+
+func is_stack() -> bool:
+	return _stack_icon.visible
+
+
+func set_appearance(new_value: int) -> void:
+	if new_value < ButtonType.VERTICAL or new_value > ButtonType.HORIZONTAL:
+		push_error("Invalid value '%d' for asset button appearance" % new_value)
+		return
+	
+	_button_vertical.visible = (new_value == ButtonType.VERTICAL)
+	_button_horizontal.visible = (new_value == ButtonType.HORIZONTAL)
+
+
+func set_text(new_value: String) -> void:
+	_button_vertical.vertical_text = new_value
+	_button_horizontal.text = new_value
+
+
+func set_texture(new_value: Texture) -> void:
+	_button_vertical.texture = new_value
+	_button_horizontal.icon = new_value
+
+
+func set_hint(new_value: String) -> void:
+	_button_vertical.hint_tooltip = new_value
+	_button_horizontal.hint_tooltip = new_value
+
+
+func set_font_override(new_value: Font) -> void:
+	_button_vertical.font_override = new_value
+	_button_horizontal.add_font_override("font", new_value)
+
+
+func set_folder(new_value: bool) -> void:
+	_folder_icon.visible = new_value
+
+
+func set_stack(new_value: bool) -> void:
+	_stack_icon.visible = new_value
+
+
+func _on_button_pressed():
+	emit_signal("pressed")
---
+++ game/ui/asset_menu/asset_grid.gd
@@ -0,0 +1,277 @@
+# tabletop-club
+# Copyright (c) 2020-2024 Benjamin 'drwhut' Beddows.
+# Copyright (c) 2021-2024 Tabletop Club contributors (see game/CREDITS.tres).
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+class_name AssetGrid
+extends GridContainer
+
+## A grid of [AssetButton].
+
+
+## Fired when the user has pressed a folder button.
+signal node_selected(node_id)
+
+## Fired when the user has pressed an asset button.
+signal asset_selected(asset_entry)
+
+
+## The different layouts that the asset grid can have.
+enum GridLayout {
+	ICON,
+	LIST,
+}
+
+
+## The number of characters that can be on one line in a tooltip before a new
+## line needs to be used.
+const MAX_CHARS_PER_HINT_LINE := 80
+
+
+## The layout that the asset grid uses to display assets.
+export(GridLayout) var layout := GridLayout.LIST setget set_layout
+
+## The zoom level for the grid, which determines how big the buttons are.
+export(int, 3) var zoom := 1 setget set_zoom
+
+
+## The [AssetNode] whose contents the grid will display when refreshed.
+var asset_node: AssetNode = null
+
+
+## Refresh the grid to display the contents of [member asset_node] with the
+## current settings. This will queue the current nodes to be deleted, and create
+## a new set to be added to the scene tree.
+func refresh() -> void:
+	if asset_node == null:
+		push_error("Cannot refresh grid, asset_node is null")
+		return
+	
+	for element in get_children():
+		var child: Node = element
+		child.queue_free()
+	
+	# Display directories first.
+	# TODO: Make sure these arrays are sorted first, depending on how the user
+	# wants us to sort them.
+	for element in asset_node.get_children():
+		var child_node: AssetNode = element
+		
+		var dir_button := AssetButton.new()
+		dir_button.name = child_node.id
+		# TODO: Make sure changing the locale globally changes the name.
+		dir_button.text = _get_node_name(child_node.id)
+		# TODO: Set button icon.
+		dir_button.hint = _get_node_hint(child_node.id)
+		dir_button.folder = true
+		dir_button.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+		dir_button.connect("pressed", self, "_on_node_button_pressed",
+				[ child_node.id ])
+		add_child(dir_button)
+	
+	# Display assets after.
+	for element in asset_node.entry_list:
+		var asset_entry: AssetEntry = element
+		
+		var asset_button := AssetButton.new()
+		asset_button.name = asset_entry.id
+		asset_button.text = asset_entry.name
+		# TODO: Create thumbnails for assets, use here.
+		asset_button.hint = _add_newlines(asset_entry.desc)
+		asset_button.stack = (asset_entry is AssetEntryCollection)
+		asset_button.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+		asset_button.connect("pressed", self, "_on_asset_button_pressed",
+				[ asset_entry ])
+		add_child(asset_button)
+	
+	# Set the appearance of the new buttons.
+	_apply_layout_and_zoom()
+
+
+func set_layout(new_value: int) -> void:
+	if new_value < GridLayout.ICON or new_value > GridLayout.LIST:
+		push_error("Invalid value '%d' for grid layout" % new_value)
+		return
+	
+	layout = new_value
+	_apply_layout_and_zoom()
+
+
+func set_zoom(new_value: int) -> void:
+	if new_value < 0 or new_value > 3:
+		push_error("Invalid value '%d' for grid zoom" % new_value)
+		return
+	
+	zoom = new_value
+	_apply_layout_and_zoom()
+
+
+# Use the configured layout and zoom to adjust the appearance of the buttons.
+func _apply_layout_and_zoom() -> void:
+	var button_type := 0
+	var button_height := 0.0
+	var button_font := preload("res://fonts/res/asset_menu_font.tres")
+	match layout:
+		GridLayout.ICON:
+			button_type = AssetButton.ButtonType.VERTICAL
+			
+			match zoom:
+				0:
+					columns = 5
+					button_height = 100.0
+				1:
+					columns = 4
+					button_height = 125.0
+				2:
+					columns = 3
+					button_height = 167.0
+					button_font = preload("res://fonts/res/asset_menu_font_major.tres")
+				3:
+					columns = 2
+					button_height = 250.0
+					button_font = preload("res://fonts/res/asset_menu_font_major.tres")
+		GridLayout.LIST:
+			button_type = AssetButton.ButtonType.HORIZONTAL
+			
+			match zoom:
+				0:
+					columns = 3
+					button_height = 50.0
+				1:
+					columns = 2
+					button_height = 50.0
+				2:
+					columns = 2
+					button_height = 80.0
+					button_font = preload("res://fonts/res/asset_menu_font_major.tres")
+				3:
+					columns = 1
+					button_height = 80.0
+					button_font = preload("res://fonts/res/asset_menu_font_major.tres")
+	
+	for element in get_children():
+		var button: AssetButton = element
+		button.appearance = button_type
+		button.font_override = button_font
+		button.rect_min_size = Vector2(0.0, button_height)
+
+
+# Get the translated name for nodes with the given ID.
+func _get_node_name(node_id: String) -> String:
+	match node_id:
+		AssetTree.ALL_PACKS_ID:
+			return tr("All Packs")
+		AssetTree.INDIVIDUAL_CARDS_ID:
+			return tr("Individual Cards")
+		AssetTree.OTHER_DICE_ID:
+			return tr("Other")
+		AssetTree.INDIVIDUAL_TOKENS_ID:
+			return tr("Individual Tiles / Tokens")
+		"boards":
+			return tr("Boards")
+		"cards":
+			return tr("Cards")
+		"containers":
+			return tr("Containers")
+		"dice":
+			return tr("Dice")
+		"music":
+			return tr("Music")
+		"pieces":
+			return tr("Pieces")
+		"sounds":
+			return tr("Sound Effects")
+		"speakers":
+			return tr("Speakers")
+		"timers":
+			return tr("Timers")
+		"tokens":
+			return tr("Tiles / Tokens")
+		_:
+			# Check to see if the ID is that of an asset pack.
+			for element in AssetDB.get_all_packs():
+				var pack: AssetPack = element
+				if pack.id == node_id:
+					return pack.name
+			
+			push_error("Could not find name for node with ID '%s'" % node_id)
+			return "#ERROR#"
+
+
+# Get the translated tooltip hint for nodes with the given ID.
+func _get_node_hint(node_id: String) -> String:
+	match node_id:
+		AssetTree.ALL_PACKS_ID:
+			return tr("Browse assets from all imported asset packs.")
+		AssetTree.INDIVIDUAL_CARDS_ID:
+			return tr("Browse individual cards, instead of pre-made decks.")
+		AssetTree.OTHER_DICE_ID:
+			return tr("Browse dice with non-standard numbers of faces.")
+		AssetTree.INDIVIDUAL_TOKENS_ID:
+			return tr("Browse individual tiles and tokens, instead of pre-made stacks.")
+		"boards":
+			return tr("Boards are used as a surface to place objects on.")
+		"cards":
+			return tr("Cards can be stacked on top of each other, and placed in your hand.")
+		"containers":
+			return tr("Containers can hold any number of other objects.")
+		"dice":
+			return tr("Dice can be rolled to create random values.")
+		"music":
+			# TODO: Revisit hint for music and sounds, are they still accurate
+			# after v0.2.0 re-write?
+			return tr("Music consists of audio tracks that are played on repeat.")
+		"pieces":
+			return tr("Pieces are generic objects with no special functionality.")
+		"sounds":
+			return tr("Sound effects are audio tracks that are played one-off.")
+		"speakers":
+			return tr("Speakers can play music or sound effects.")
+		"timers":
+			return tr("Timers can be used in stopwatch, timer, or clock mode.")
+		"tokens":
+			return tr("Tiles and tokens can be stacked on top of each other.")
+		_:
+			return ""
+
+
+# Add new-line characters to a hint if they are needed to make sure that the
+# entire hint is visible on the screen.
+func _add_newlines(text: String) -> String:
+	var line_length := 0
+	for index in range(text.length()):
+		var c := text[index]
+		if c == "\n":
+			line_length = 0
+		else:
+			line_length += 1
+			if c == " " and line_length >= MAX_CHARS_PER_HINT_LINE:
+				text[index] = "\n"
+				line_length = 0
+	
+	return text
+
+
+func _on_asset_button_pressed(asset_entry: AssetEntry):
+	emit_signal("asset_selected", asset_entry)
+
+
+func _on_node_button_pressed(node_id: String):
+	emit_signal("node_selected", node_id)
