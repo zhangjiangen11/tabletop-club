0ad2d2bb78c572c427e2f5cfab368d75d14779aa
drwhut <drwhut@gmail.com>
2024-09-12 19:00:29 +0100

    Added back and forward buttons to the asset menu.

--- game/ui/asset_menu/asset_window.gd
+++ game/ui/asset_menu/asset_window.gd
@@ -29,6 +29,11 @@ extends WindowDialog
 signal asset_selected(asset_entry)
 
 
+## The maximum size of the back and front stacks, used when pressing either the
+## back or front button.
+const DIR_STACK_MAX_SIZE := 50
+
+
 ## The top-level directory that this window uses.
 ## This is used to specify which types of assets the user can choose from.
 export(String) var category := ""
@@ -42,6 +47,15 @@ export(String) var default_path := "/%ALL_PACKS%"
 var current_path: String setget set_current_path
 
 
+# The stack used when going back through previously visited directories.
+var _back_stack: Array = []
+
+# The stack used to go forward through directories after going back.
+var _forward_stack: Array = []
+
+
+onready var _back_button: Button = $MainContainer/ButtonContainer/BackButton
+onready var _forward_button: Button = $MainContainer/ButtonContainer/ForwardButton
 onready var _asset_breadcrumb: AssetBreadcrumb = $MainContainer/BreadcrumbScrollContainer/AssetBreadcrumb
 onready var _asset_grid: AssetGrid = $MainContainer/AssetScrollContainer/AssetGrid
 
@@ -95,6 +109,30 @@ func set_current_path(new_value: String) -> void:
 	_asset_grid.refresh()
 
 
+# Push a path to the back stack, enabling the back button.
+func _push_back_stack(dir_path: String, clear_forward: bool = true) -> void:
+	_back_stack.push_back(dir_path)
+	_back_button.disabled = false
+	
+	if clear_forward:
+		_forward_stack.clear()
+		_forward_button.disabled = true
+	
+	# If the stack is now too big, pop elements from the bottom of the stack.
+	while _back_stack.size() > DIR_STACK_MAX_SIZE:
+		_back_stack.pop_front()
+
+
+# Push a path to the forward stack, enabling the forward button.
+func _push_forward_stack(dir_path: String) -> void:
+	_forward_stack.push_back(dir_path)
+	_forward_button.disabled = false
+	
+	# If the stack is now too big, pop elements from the bottom of the stack.
+	while _forward_stack.size() > DIR_STACK_MAX_SIZE:
+		_forward_stack.pop_front()
+
+
 func _on_AssetTree_tree_generated():
 	if AssetTree.get_asset_node(current_path) == null:
 		# The node we were displaying no longer exists, so revert back to the
@@ -105,7 +143,43 @@ func _on_AssetTree_tree_generated():
 		_asset_grid.refresh()
 
 
+func _on_BackButton_pressed():
+	# Pop the back stack until we find a valid path - this is because the tree
+	# may have changed since the stack was last pushed to.
+	while not _back_stack.empty():
+		var path: String = _back_stack.pop_back()
+		if AssetTree.get_asset_node(path) == null:
+			continue
+		
+		_push_forward_stack(current_path)
+		
+		set_current_path(path)
+		break
+	
+	if _back_stack.empty():
+		_back_button.disabled = true
+
+
+func _on_ForwardButton_pressed():
+	while not _forward_stack.empty():
+		var path: String = _forward_stack.pop_back()
+		if AssetTree.get_asset_node(path) == null:
+			continue
+		
+		_push_back_stack(current_path, false)
+		
+		set_current_path(path)
+		break
+	
+	if _forward_stack.empty():
+		_forward_button.disabled = true
+
+
 func _on_AssetBreadcrumb_dir_selected(dir_path: String):
+	if dir_path == current_path:
+		return
+	
+	_push_back_stack(current_path)
 	set_current_path(dir_path)
 
 
@@ -119,6 +193,8 @@ func _on_AssetGrid_node_selected(node_id: String):
 	if asset_node == null:
 		push_error("Cannot set path to '%s', node does not exist" % new_path)
 		return
+	
+	_push_back_stack(current_path)
 	
 	current_path = new_path
 	_asset_breadcrumb.append_dir(new_path)
